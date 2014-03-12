08133838ce869976c9ff4912b9b71704d190dca9
Edward Paget <ed.paget@gmail.com>
2014-03-12 15:47:58 -0500

    Write kafka's pid during startup

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,5 @@ default['kafka']['version'] = '0.8.1'
 default['kafka']['src_url'] = "http://apache.cs.utah.edu/kafka/0.8.1/kafka-0.8.1-src.tgz"
 default['kafka']['install_directory'] = "/opt/kafka"
 default['kafka']['number_of_brokers'] = 1
-default['kafka']['zookeeper_hosts'] = ['localhost:2181']
+default['kafka']['zookeeper_hosts'] = []
 default['kafka']['hostname'] = false
